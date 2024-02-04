ebbae6eab32de32d977f95e72dad994a5cdb5097
Dimitri Heinrich <dimi-mail@gmx.de>
2024-02-04 21:57:20 +0100

    Task for the team

---
+++ src/CurrencyExchange.java
@@ -0,0 +1,12 @@
+public class CurrencyExchange {
+    private static final double USD_TO_EUR_RATE = 0.85;
+    private static final double USD_TO_GBP_RATE = 0.73;
+
+
+        /* TODO
+        Разработать все классы из папки src.Добавить другие валюты. Внести условия и исключения.
+        Создать класс Main
+         */
+
+
+}
