62d431aa5e945a74b3a20a6bc02633e397d29283
Liliana Platon <l.platon@web.de>
2024-02-05 11:35:29 +0100

    add exchange and request classes

--- src/ExchangeHistory.java
+++ src/ExchangeHistory.java
@@ -1,2 +1,17 @@
+
+  import java.util.ArrayList;
+    import java.util.List;
 public class ExchangeHistory {
+  private List<ExchangeRecord> history;
+  public ExchangeHistory() {
+    this.history = new ArrayList<>();
+  }
+  public void addRecord(ExchangeRecord record) {
+    history.add(record);
+  }
+  public void displayHistory() {
+    for (ExchangeRecord record : history) {
+      System.out.println(record);
+    }
+  }
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
 public class UserInterface {
+  private Scanner scanner;
+  public UserInterface() {
+    this.scanner = new Scanner(System.in);
+  }
+  public ExchangeRequest getUserInput() {
+    System.out.println("Введите сумму для обмена:");
+    double amount = scanner.nextDouble();
+    System.out.println("Введите валюту, которую вы хотите обменять (например, USD):");
+    String fromCurrency = scanner.next();
+    System.out.println("Введите валюту, которую вы хотите приобрести (например, EUR):");
+    String toCurrency = scanner.next();
+    return new ExchangeRequest(amount, fromCurrency, toCurrency);
+  }
+  public void displayResult(double result) {
+    System.out.println("Результат обмена: " + result);
+  }
+  public Scanner getScanner() {
+    return scanner;
+  }
 }
