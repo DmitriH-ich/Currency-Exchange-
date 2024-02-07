40b23c5a180886e2aea7be253780fa9117dc356c
Dimitri Heinrich <dimi-mail@gmx.de>
2024-02-07 09:18:48 +0100

    before merge

--- src/CurrencyExchange.java
+++ src/CurrencyExchange.java
@@ -3,6 +3,7 @@
   public class CurrencyExchange {
     private static final double USD_TO_EUR_RATE = 0.85;
     private static final double USD_TO_GBP_RATE = 0.73;
+
     public double performExchange(ExchangeRequest request) {
       double amount = request.getAmount();
       String fromCurrency = request.getFromCurrency();
@@ -13,3 +14,4 @@
       } else if ("USD".equals(fromCurrency) && "GBP".equals(toCurrency)) {
         result = amount * USD_TO_GBP_RATE;
       }
+    }
